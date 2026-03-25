66e42f42e313cb0fcafe7cc2ecd28c2535ecaec6
UTTAM KUMAR RAJWAR <uttamrajwar.92@gmail.com>
2026-03-25 13:34:18 +0530

    added frontend link to cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const errorMiddleware = require('./Middlewares/error-middleware');
 const app = express();
 
 const corsOptions = {
-    origin:"http://localhost:5173",
+    origin:"https://todomeru.netlify.app/",
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
 }
