8dedbebdf0006802296d7f8e10f91c1755f6287e
UTTAM KUMAR RAJWAR <uttamrajwar.92@gmail.com>
2026-03-27 17:52:26 +0530

    mailer.js modify

--- utils/mailer.js
+++ utils/mailer.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
  host: "smtp.gmail.com",
-  port: 587,
+  port: 465,
   secure: false, // VERY IMPORTANT
   auth: {
     user: process.env.EMAIL_USER,     // Your email
